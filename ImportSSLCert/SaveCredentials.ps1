595575d21525b47c85cbdb54feaf5f9148a07cd4
fmsadmin <ernest.koe@proofgroup.com>
2019-06-25 09:26:43 -0400

    added default path and filename in SaveCredentials.ps1

--- ImportSSLCert/SaveCredentials.ps1
+++ ImportSSLCert/SaveCredentials.ps1
@@ -1,29 +1,42 @@
-$DEFAULT_PATH = "/Program Files/FileMaker/FileMaker Server/CStore"
+$DEFAULT_PATH = "C:\Program Files\FileMaker\FileMaker Server\CStore"
+$DEFAULT_CRED_FILENAME = "WacsFMSCreds.xml"
 
 function Save-Password {
 
     param(
         [Parameter(Mandatory=$false)]
         [string]
-        $filename
+        $Filename,
+
+        [Parameter(Mandatory=$false)]
+        [string]
+        $CredsPath
     )
 
-    if (!$filename) {
-        $filename = Read-Host "Enter filename, (defaults to 'fms_creds.xml')"
-        if ( !$filename ) { $filename = "fms_creds.xml" }
-        Write-Host $filename
+    if (!$Filename) {
+        $Filename = Read-Host "Enter filename, (WacsFMSCreds.xml)"
+        if ( !$Filename ) { $Filename = $DEFAULT_CRED_FILENAME }
+        # Write-Host $Filename
     }
+   
+    if (!$CredsPath) {
+        $CredsPath = Read-Host "Save $Filename to: ($DEFAULT_PATH)"
+        if ( !$CredsPath ) { $CredsPath = $DEFAULT_PATH }
+    }
+ 
+    #Get credentials securely
+    $Credentials = Get-Credential -message "What is the FileMaker Server admin user and password?"
 
-    $credentials = Get-Credential -message "What is the FileMaker Server admin user and password?"
-
-    $path =  Join-Path -Path $DEFAULT_PATH -ChildPath $filename
-    $credentials | Export-CliXml $path
+    $Path =  Join-Path -Path $CredsPath -ChildPath $Filename
+    $Credentials | Export-CliXml $Path
 
     #Test to make sure our creds are good
-     $ic = Import-CliXml $path
+     # $ic = Import-CliXml $Path
      # $Password = $ic.Password | ConvertFrom-SecureString
-     $PlainTextPassword = $ic.GetNetworkCredential().Password
-     Write-Host "Test:" $ic.Username $PlainTextPassword
+     # $PlainTextPassword = $ic.GetNetworkCredential().Password
+     # Write-Host "Saved:" $ic.Username $PlainTextPassword
+
+     Write-Host "Saved to $Path"
 }
  
 Save-Password
